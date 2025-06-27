a8562dd35f94c652167ad7502ea1cb9a6ff0ace2
Jose <josecueto181@gmail.com>
2025-06-26 20:52:19 -0500

    footer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,3 +4,22 @@ const menu = document.querySelector(".menu_horizontal");
 btn.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("formulario-contacto");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const correo = document.getElementById("correo").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    if (!nombre || !correo || !mensaje) {
+      alert("Por favor completa todos los campos antes de enviar el formulario.");
+      return;
+    }
+    alert("Formulario enviado correctamente ✅");
+    form.reset();
+  });
+});
