1e7bdcdc280e22e36e8e78b2c98c8314e4ad879b
Jose <josecueto181@gmail.com>
2025-07-03 22:28:16 -0500

    register

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -28,3 +28,46 @@ document.addEventListener("DOMContentLoaded", function () {
     form.reset();
   });
 });
+
+// register
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("formulario-register");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const nombre = document.getElementById("nombre").value.trim();
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      if (!nombre || !email || !password) {
+        alert("Por favor completa todos los campos.");
+        return;
+      }
+      alert("Registro exitoso");
+      form.reset();
+
+      setTimeout(() => {
+        window.location.href = "../pages/login.html";
+      }, 1000);
+    });
+  }
+});
+
+// olvide
+document.getElementById("formulario-olvide").addEventListener("submit", function (e) {
+  e.preventDefault();
+    const email = document.getElementById("email").value.trim();
+    if (!email) {
+      alert("Por favor ingresa tu correo electrónico.");
+      return;
+    }
+    alert("Hemos enviado un enlace de recuperación a tu correo.");
+    this.reset();
+    setTimeout(() => {
+      window.location.href = "../pages/login.html";
+    }, 1000);
+});
+
+
